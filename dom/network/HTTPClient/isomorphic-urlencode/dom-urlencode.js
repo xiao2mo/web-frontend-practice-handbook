f66eb93238ad7e766ba877a29094ad136103b40d
wxyyxc1992 <hoteam@live-forest.com>
2016-09-08 21:04:33 +0800

    修改DOM系列文章

---
+++ dom/network/HTTPClient/isomorphic-urlencode/dom-urlencode.js
@@ -0,0 +1,16 @@
+/*
+* @Author: wxyyxc1992
+* @Date:   2016-09-08 20:26:16
+* @Last Modified by:   wxyyxc1992
+* @Last Modified time: 2016-09-08 20:28:39
+*/
+
+'use strict';
+
+function urlencode(url,encode){
+
+	if(!encode){
+		encode = "gbk";
+	}
+
+}
---
+++ dom/network/HTTPClient/isomorphic-urlencode/node-urlencode.js
@@ -0,0 +1,12 @@
+/*
+* @Author: wxyyxc1992
+* @Date:   2016-09-08 20:26:06
+* @Last Modified by:   wxyyxc1992
+* @Last Modified time: 2016-09-08 20:27:15
+*/
+
+'use strict';
+
+var urlencode = require('urlencode');
+
+module.exports = urlencode;
