d791834f2370f7641511174b6f33d25aa837441e
张梓雄 <zhangzixiong@1001hao.com>
2017-03-10 12:29:46 +0800

    添加 React Router 4 样例

--- OpenSource/frontend-playground/react-router-4/src/App.js
+++ OpenSource/frontend-playground/react-router-4/src/App.js
@@ -7,16 +7,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h2>Welcome to React Router 4</h2>
-        </div>
-        <p className="App-intro">
-          {/*基本示例*/}
-          {/*<BasicExample/>*/}
-          {/*转场动画*/}
-          <AnimationExample/>
-        </p>
+        {/*基本示例*/}
+        <BasicExample />
+        {/*转场动画*/}
+        <AnimationExample />
       </div>
     );
   }
